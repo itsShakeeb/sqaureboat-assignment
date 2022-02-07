1cadf194d3fff8bb0c928a0554f2d46bc6003b4d
shakeeb <shakeebadab50@gmail.com>
2022-02-07 21:52:29 +0530

    bug fixed

--- src/utils/APIService/index.js
+++ src/utils/APIService/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: `${process.env.REACT_APP_BASE_URL}`,
+  baseURL: "https://jobs-api.squareboat.info/api/v1/",
 });
 
 axiosInstance.interceptors.request.use(
